0b6ec32d6d656bf728230c713e109b063d5978cb
Ryo Nakamura <r7kamura@gmail.com>
2012-12-16 16:55:59 +0900

    Fix parser bug in multi line comment

--- lib/cssdoc/parser.rb
+++ lib/cssdoc/parser.rb
@@ -6,20 +6,11 @@ module Cssdoc
 
     def parse
       lines.each do |line|
-        if current_section
-          if line.comment?
-            current_section << line.stripped_content
-          end
-          if line.end_multi_comment? || !line.comment?
-            sections << current_section
-            self.current_section = nil
-          end
-        else
-          if line.begin_multi_comment? || line.single_comment?
-            self.current_section = Section.new
-            current_section << line.stripped_content
-          end
-        end
+        begin_multi_comment  if line.begin_multi_comment?
+        begin_single_comment if !in_comment && !current_section && line.comment?
+        add_line(line)       if in_comment || line.comment?
+        end_single_comment   if !in_comment && current_section && !line.comment?
+        end_multi_comment    if line.end_multi_comment?
       end
       Document.new(path, sections)
     end
@@ -28,6 +19,7 @@ module Cssdoc
 
     attr_reader :path
     attr_accessor :current_section
+    attr_accessor :in_comment
 
     def lines
       path.each_line.map {|line| Line.new(line) }
@@ -35,6 +27,29 @@ module Cssdoc
 
     def sections
       @sections ||= []
+    end
+
+    def begin_multi_comment
+      begin_single_comment
+      self.in_comment = true
+    end
+
+    def begin_single_comment
+      self.current_section = Section.new
+    end
+
+    def add_line(line)
+      current_section << line.stripped_content
+    end
+
+    def end_single_comment
+      sections << current_section
+      self.current_section = nil
+    end
+
+    def end_multi_comment
+      end_single_comment
+      self.in_comment = nil
     end
   end
 end
